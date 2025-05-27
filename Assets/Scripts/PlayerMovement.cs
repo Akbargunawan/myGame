5e9209dc14ef4f563eb342174b737c254103742d
Akbargunawan <123akbargunawan@gmail.com>
2025-05-27 15:39:48 +0700

    newcommit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float moveSpeed = 5f;
+    public float runSpeed = 8f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private PlayerController playerController;
+
+    private float mobileInputX = 0f;
+    private Vector2 moveInput;
+    private bool isJumping = false;
+
+    private enum MovementState { idle, walk, jump, fall, run }
+
+    [Header("Jump Settings")]
+    [SerializeField] private LayerMask jumpableGround;
+    private BoxCollider2D coll;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+
+        playerController = new PlayerController();
+    }
+
+    private void OnEnable()
+    {
+        playerController.Enable();
+
+        playerController.Movement.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        playerController.Movement.Move.canceled += ctx => moveInput = Vector2.zero;
+        playerController.Movement.Jump.performed += ctx => Jump();
+    }
+
+    private void OnDisable()
+    {
+        playerController.Disable();
+    }
+
+    private void Update()
+    {
+        if (Application.isMobilePlatform)
+        {
+            moveInput = new Vector2(mobileInputX, 0f);
+        }
+        else
+        {
+            moveInput = playerController.Movement.Move.ReadValue<Vector2>();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2((moveInput.x + mobileInputX) * moveSpeed, rb.velocity.y);
+        rb.velocity = targetVelocity;
+
+        UpdateAnimation();
+
+        if (isGrounded() && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            isJumping = false;
+        }
+    }
+
+    private void UpdateAnimation()
+    {
+        MovementState state;
+
+        float horizontal = moveInput.x != 0 ? moveInput.x : mobileInputX;
+
+        if (horizontal > 0f)
+        {
+            state = MovementState.walk;
+            sprite.flipX = false;
+        }
+        else if (horizontal < 0f)
+        {
+            state = MovementState.walk;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > 0.1f)
+        {
+            state = MovementState.jump;
+        }
+        else if (rb.velocity.y < -0.1f)
+        {
+            state = MovementState.fall;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+        }
+    }
+
+    public void MoveRight(bool isPressed)
+    {
+        if (isPressed)
+            mobileInputX = 1f;
+        else if (mobileInputX == 1f)
+            mobileInputX = 0f;
+    }
+
+    public void MoveLeft(bool isPressed)
+    {
+        if (isPressed)
+            mobileInputX = -1f;
+        else if (mobileInputX == -1f)
+            mobileInputX = 0f;
+    }
+
+    public void MobileJump()
+    {
+        if (isGrounded())
+        {
+            Jump();
+        }
+    }
+}
